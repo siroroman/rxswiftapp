a4ff56c47c17e42a9710510ae5c1fed8f4a85d4a
Roman Siro <siroroman@gmail.com>
2019-05-28 18:02:42 +0200

    files added

---
+++ Podfile
@@ -0,0 +1,14 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'rxswiftapp' do
+  use_frameworks!
+
+  # Pods for rxswiftapp
+
+  pod 'RxCocoa'
+  pod 'RxSwift'
+  pod 'RxAlamofire'
+  pod 'RxRealm'
+
+end
---
+++ rxswiftapp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  rxswiftapp
+//
+//  Created by Roman Siro on 28/05/2019.
+//  Copyright © 2019 Roman Siro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
